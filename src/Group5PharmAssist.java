ac192730986b7ae745c6a7ca6c9306b23efd0e71
samarpan-ux <rawalsamarpan@gmail.com>
2025-12-16 08:41:19 +0545

    add code

--- src/Group5PharmAssist.java
+++ src/Group5PharmAssist.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
+import view.Signup;
 /**
  *
  * @author kritss
@@ -14,6 +14,7 @@ public class Group5PharmAssist {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new Signup().setVisible(true);
     }
     
 }
